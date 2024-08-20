2e4c7d10bd5a1555e25755e9047c925b9836515c
god <naughty7878@163.com>
2024-08-21 00:10:47 +0800

    python学习

---
+++ python爬虫/chaojiying.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import requests
+from hashlib import md5
+
+
+class Chaojiying_Client(object):
+
+    def __init__(self, username, password, soft_id):
+        self.username = username
+        password = password.encode('utf8')
+        self.password = md5(password).hexdigest()
+        self.soft_id = soft_id
+        self.base_params = {
+            'user': self.username,
+            'pass2': self.password,
+            'softid': self.soft_id,
+        }
+        self.headers = {
+            'Connection': 'Keep-Alive',
+            'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0)',
+        }
+
+    def PostPic(self, im, codetype):
+        """
+        im: 图片字节
+        codetype: 题目类型 参考 http://www.chaojiying.com/price.html
+        """
+        params = {
+            'codetype': codetype,
+        }
+        params.update(self.base_params)
+        files = {'userfile': ('ccc.jpg', im)}
+        r = requests.post('http://upload.chaojiying.net/Upload/Processing.php', data=params, files=files,
+                          headers=self.headers)
+        return r.json()
+
+    def PostPic_base64(self, base64_str, codetype):
+        """
+        im: 图片字节
+        codetype: 题目类型 参考 http://www.chaojiying.com/price.html
+        """
+        params = {
+            'codetype': codetype,
+            'file_base64': base64_str
+        }
+        params.update(self.base_params)
+        r = requests.post('http://upload.chaojiying.net/Upload/Processing.php', data=params, headers=self.headers)
+        return r.json()
+
+    def ReportError(self, im_id):
+        """
+        im_id:报错题目的图片ID
+        """
+        params = {
+            'id': im_id,
+        }
+        params.update(self.base_params)
+        r = requests.post('http://upload.chaojiying.net/Upload/ReportError.php', data=params, headers=self.headers)
+        return r.json()
+
+
+def parse_pic_str(img_path):
+    chaojiying = Chaojiying_Client('admin', '123456', '123')
+    with open(img_path, 'rb') as file:
+        im = file.read()
+        resp = chaojiying.PostPic(im, 1902)
+    return resp.get('pic_str')
+
+
+if __name__ == '__main__':
+    # 用户中心>>软件ID 生成一个替换 96001
+    chaojiying = Chaojiying_Client('admin', '123456', '123')
+    # 本地图片文件路径 来替换 a.jpg 有时WIN系统须要//
+    im = open('downloads/my_image.jpg', 'rb').read()
+    # 1902 验证码类型  官方网站>>价格体系 3.4+版 print 后要加()
+    resp = chaojiying.PostPic(im, 1902)
+    print(resp)
+    print(resp.get('pic_str'))
+    # {'err_no': -10023, 'err_str': '认证失败', 'pic_id': '0', 'pic_str': '', 'md5': ''}
+    # print chaojiying.PostPic(base64_str, 1902)  #此处为传入 base64代码
---
+++ python爬虫/爬虫26-urllib-reqeusts-cookie登录古诗文网.py
@@ -0,0 +1,146 @@
+import json
+import os
+import chaojiying
+
+import requests
+from lxml import etree
+
+
+def download_image(session, url, save_path):
+    try:
+        # 发送 GET 请求
+        response = session.get(url, stream=True)
+
+        # 检查请求是否成功
+        response.raise_for_status()
+
+        # 确保文件夹存在
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+        # 以二进制写模式打开文件
+        with open(save_path, 'wb') as file:
+            # 逐块写入文件
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+
+        print(f"图片已成功下载到: {save_path}")
+
+    except requests.RequestException as e:
+        print(f"下载图片时发生错误: {e}")
+
+
+def send_get_request(url, params=None, headers=None):
+    try:
+        # 发送 GET 请求
+        response = requests.get(url, params=params, headers=headers)
+
+        # 检查请求是否成功
+        response.raise_for_status()
+        response.encoding = 'utf-8'
+        return response.text
+
+        # 尝试解析 JSON 响应
+        # try:
+        #     json_response = response.json()
+        #     return json_response
+        # except ValueError:
+        #     # 如果响应不是 JSON 格式，返回文本内容
+        #     return response.text
+
+    except requests.RequestException as e:
+        print(f"发生错误: {e}")
+        return None
+
+
+def send_post_request(session, url, data, headers=None):
+    try:
+        # 发送 POST 请求
+        response = session.post(url, data=data, headers=headers)
+
+        # 检查请求是否成功
+        response.raise_for_status()
+
+        # 解决乱码
+        response.encoding = 'utf-8'
+        return response.text
+
+        # # 尝试解析 JSON 响应
+        # try:
+        #     json_response = response.json()
+        #     return json_response
+        # except json.JSONDecodeError:
+        #     # 如果响应不是 JSON 格式，返回文本内容
+        #     return response.text
+
+    except requests.RequestException as e:
+        print(f"发生错误: {e}")
+        return None
+
+
+# 使用示例
+'''
+__VIEWSTATE: 7gH1CGr857gvKfh2vJZgnUffcsrQaFOoubyeWRvAU1rLxLxej6ejzvS1cR9/3gZ2qFq0Qr/jQ/TKhqeYi71jJNokqOD44UFNZ8U/PMp4GQP/ej8QIwPCm5o9DAbPgk2bb9Pjt3azPIlVCPjvCOtfO+3Ag2s=
+__VIEWSTATEGENERATOR: C93BE1AE
+from: http://www.gushiwen.cn/user/collect.aspx
+email: 11111111@qq.com
+pwd: 123456
+code: 1234
+denglu: 登录
+'''
+
+# url = 'https://www.gushiwen.cn/user/login.aspx?from=http://www.gushiwen.cn/user/collect.aspx'
+url = 'https://www.gushiwen.cn/user/login.aspx'
+
+params = {
+    'from': 'http://www.gushiwen.cn/user/collect.aspx',
+}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+    # 'Authorization': 'Bearer YOUR_TOKEN_HERE'  # 如果需要认证
+}
+
+response = send_get_request(url, params, headers)
+# print("响应:", response)
+# 解析
+tree = etree.HTML(response)
+
+# 获取元素
+viewstate = tree.xpath('//input[@id="__VIEWSTATE"]/@value')[0]
+viewstategenrator = tree.xpath('//input[@id="__VIEWSTATEGENERATOR"]/@value')[0]
+# print('__VIEWSTATE', viewstate)
+# print('__VIEWSTATEGENERATOR', viewstategenrator)
+
+# 获取验证码图片
+code = tree.xpath('//img[@id="imgCode"]/@src')[0]
+img_url = f'https://www.gushiwen.cn{code}'
+# print('img_url', img_url)
+
+# ===========图片请求要用登录请求共用session==============
+session = requests.session()
+
+# 图片保存路径
+save_path = 'downloads/my_image.jpg'
+download_image(session, img_url, save_path)
+
+# 下载图片，获得对应的验证码
+code_name = input('请输入你的验证码：')
+# code_name = chaojiying.parse_pic_str(save_path)
+
+# 登录url
+login_url = 'https://www.gushiwen.cn/user/login.aspx?from=http%3a%2f%2fwww.gushiwen.cn%2fuser%2fcollect.aspx'
+
+form_data = {
+    '__VIEWSTATE': '',
+    '__VIEWSTATEGENERATOR': 'C93BE1AE',
+    'from': 'http://www.gushiwen.cn/user/collect.aspx',
+    'email': 'naughty7878@163.com',
+    'pwd': 'qq657370713967',
+    'code': code_name,
+    'denglu': '登录'
+}
+
+response_post = send_post_request(session, login_url, form_data, headers)
+print(response_post)
+with open('gushiwen.html', 'w', encoding='utf-8') as file:
+    file.write(response_post)
+
